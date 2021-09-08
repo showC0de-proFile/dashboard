06d55c5245db8b0cca5356bdb843706027aa311c
tsmsp <tsm_sp@hotmail.com>
2021-09-08 11:31:10 -0300

    themes: dark and white

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,25 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#212129',
+        secondary: '#35373f',
+        tertiary: '#2a2c34',
+        quartenary: '#e9e9ef',
+        
+
+        white: '#fff',
+        black: '#000',
+        middleGray: '#35373f',
+        gray: '#BFBFBF',
+        grayLight: '#35373f',
+
+        colorTextButton: '#fff',
+        titleForm: '#fff',
+
+
+        success: '#2695F9',
+        info: '#F7931B',
+        warning: '#19547F',
+    },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,24 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#DCDCDC',
+        secondary: '#FFFFFF',
+        tertiary: '#F5F5F5',
+        quartenary: '#0f1014',
+
+
+        white: '#000',
+        black: '#fff',        
+        gray: '#BFBFBF',
+        middleGray: '#35373f',
+        grayLight: '#f9fafc',
+
+        colorTextButton: '#fff',
+        titleForm: '#545454', 
+
+        success: '#2695F9',
+        info: '#3B5998',
+        warning: '#19547F',
+    },
+};
