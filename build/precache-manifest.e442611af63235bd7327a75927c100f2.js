f8d319378c01d58c001827822e2e45960fb1bf7f
tsmsp <tsm_sp@hotmail.com>
2021-09-08 11:21:32 -0300

    folder build

---
+++ build/precache-manifest.e442611af63235bd7327a75927c100f2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7521859b67c43bdf8b988820257f3fe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c203b9c06c1bec432baf",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "c203b9c06c1bec432baf",
+    "url": "/static/js/2.66631462.chunk.js"
+  },
+  {
+    "revision": "8a24ea808ddcfe92831f353888548272",
+    "url": "/static/js/2.66631462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca47da2d329f16bc80df",
+    "url": "/static/js/main.c5f33be2.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  },
+  {
+    "revision": "4a2d8ad3f401ae55224b861e18c209a5",
+    "url": "/static/media/acessos-horas.4a2d8ad3.svg"
+  },
+  {
+    "revision": "fcb49e568658601ba7d5e2c13c3e6144",
+    "url": "/static/media/analytics-bg12.fcb49e56.jpg"
+  },
+  {
+    "revision": "6dda5cd3f724507dc5e06fd17ca7b478",
+    "url": "/static/media/background-indicator.6dda5cd3.jpg"
+  },
+  {
+    "revision": "c73d96b284294484747122a7777e0a7b",
+    "url": "/static/media/calendar.c73d96b2.svg"
+  },
+  {
+    "revision": "51639ddfb315ad3f74e6fae3ae3b7019",
+    "url": "/static/media/clock-2.51639ddf.svg"
+  },
+  {
+    "revision": "b85285acb52ea2047f9d89b427fa3f18",
+    "url": "/static/media/clock-time.b85285ac.svg"
+  },
+  {
+    "revision": "b0d9dfc8c1c247cd9e2947064f78da39",
+    "url": "/static/media/conclusao.b0d9dfc8.svg"
+  },
+  {
+    "revision": "85693ed47cd1cbee415982f6993e197f",
+    "url": "/static/media/crown.85693ed4.svg"
+  },
+  {
+    "revision": "a47f9091040354396fdadb2454d5727c",
+    "url": "/static/media/logo-mp-branco.a47f9091.svg"
+  },
+  {
+    "revision": "86bff31d339416c5a3d2b51c0c1e9cfe",
+    "url": "/static/media/logo-mp.86bff31d.svg"
+  },
+  {
+    "revision": "280db470291d3e24480d63517747f6d3",
+    "url": "/static/media/media.280db470.svg"
+  },
+  {
+    "revision": "bd7c1a6327f46fd93ff0daf905c7b4da",
+    "url": "/static/media/moon.bd7c1a63.svg"
+  },
+  {
+    "revision": "944de8988a8c102ee214c30ef220b2b7",
+    "url": "/static/media/ranking.944de898.svg"
+  },
+  {
+    "revision": "7484aa4c240311ee23ac1e16f72056db",
+    "url": "/static/media/star.7484aa4c.svg"
+  },
+  {
+    "revision": "e08ea2200257d471338537c3768338a1",
+    "url": "/static/media/sun.e08ea220.svg"
+  },
+  {
+    "revision": "89b7de06ab91b420dbc91fb0be1d1d28",
+    "url": "/static/media/total-acessos.89b7de06.svg"
+  }
+]);
